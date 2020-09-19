f6f5561c983eb7b4ac96bfa37e25f12b839c8a8d
hungbn <masterhung0112@gmail.com>
2020-09-19 02:29:12 +0000

    [showroom] button story use showroom

--- core/components/button/button.stories.tsx
+++ core/components/button/button.stories.tsx
@@ -2,17 +2,14 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { Button } from './button'
 import { Intent } from 'common'
+import { ShowroomWin } from 'showroom/components'
 
 storiesOf('Button', module)
   .add(
     'default',
     () => {
       return (
-        <div style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-around"            
-        }}>
+        <ShowroomWin id="story-button-default">
             {/* Empty button*/}
             <Button />
             <Button text="Hello world" />
@@ -21,7 +18,7 @@ storiesOf('Button', module)
             <Button icon="user" rightIcon="caret-down" text="Profile settings" />
 
             <Button icon="user" rightIcon="caret-down" text="Profile settings" intent={Intent.DANGER} />
-        </div>
+        </ShowroomWin>
       )
     }
   )
