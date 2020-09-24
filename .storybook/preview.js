6d4fbe7359071a574263c76b632c358c213b266c
hung <hungbn0112@gmail.com>
2020-09-24 11:48:01 +0700

    Move all source code to /src folder

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import '../styles/styles.scss'
-import '../styles/showroom/_module.scss'
+import 'styles/styles.scss'
+import 'styles/showroom/_module.scss'
 import IntlProvider from 'components/intl-provider'
-import enTranslationData from '../i18n/en.json'
+import enTranslationData from 'i18n/en.json'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
