f025fdad8eb6e09fcf270937d1e9ff57000224d9
hung <hungbn0112@gmail.com>
2020-09-24 10:11:43 +0700

    [e2e] move support, fixtures integration folder into cypress folder

--- e2e/cypress/plugins/index.js
+++ e2e/cypress/plugins/index.js
@@ -9,13 +9,18 @@
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+const log = (message) => {
+  console.log(message);
+  return null;
+};
 
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  on('task', {
+    log,
+  });
+
+  return config
 }
