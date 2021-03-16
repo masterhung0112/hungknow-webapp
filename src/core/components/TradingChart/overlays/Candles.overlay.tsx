8940fb9328aa72ecf23f3e5b3bc31970cf1c7f81
hung <hungbn0112@gmail.com>
2021-03-16 17:58:17 +0700

    Update CCandels.overlay.tsx

--- src/core/components/TradingChart/overlays/Candles.overlay.tsx
+++ src/core/components/TradingChart/overlays/Candles.overlay.tsx
@@ -2,13 +2,24 @@ import React from 'react'
 import { OverlayProps, useOverlay } from '../Overlay'
 
 export const Candles: React.FC<OverlayProps> = (props) => {
-  const { data, sub } = props
+  const { data, sub, layout } = props
   useOverlay(props)
+
+  let cnv: any = {
+    candles: [],
+    volumes: [],
+  }
 
   // If data === main candlestick data
   // render as main chart:
   if (sub === data) {
+    cnv.candles = layout.candles
+    cnv.volumes = layout.volume
+  } else {
+    cnv = layout_cnv(layout)
   }
 
-  return <></>
+  return <>
+  {cnv.candles.map}
+  </>
 }
