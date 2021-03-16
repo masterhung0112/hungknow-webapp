57afcc6afdc484eed34d7c528c1c4d61e3d609a8
hung <hungbn0112@gmail.com>
2021-03-16 11:56:32 +0700

    Update test of inputGroup

--- src/core/components/forms/inputGroup.test.tsx
+++ src/core/components/forms/inputGroup.test.tsx
@@ -1,35 +1,38 @@
 import React from 'react'
-import { mount } from 'enzyme'
 import InputGroup from './inputGroup'
 import Icon from '../icon'
 import { CssClasses } from 'common'
+import { fireEvent, render } from '@testing-library/react'
+import '@testing-library/jest-dom'
 
 describe('<InputGroup>', () => {
   it('supports custom props', () => {
-    const input = mount(<InputGroup style={{ background: 'yellow' }} tabIndex={4} />)
-    const inputElement = input.find('input')
+    const renderResult = render(<InputGroup style={{ background: 'yellow' }} tabIndex={4} />)
+    const inputElement = renderResult.container.querySelector('input')
     // Check if <input> element exists
-    expect(inputElement.exists()).toEqual(true)
+    expect(inputElement).toBeInTheDocument()
 
     // Check if the props is equal to what is input
-    const inputElementDom = inputElement.getDOMNode() as HTMLElement
+    const inputElementDom = inputElement as HTMLElement
     expect(inputElementDom.style.background).toEqual('yellow')
     expect(inputElementDom.tabIndex).toEqual(4)
   })
 
   it('would work like a text input', () => {
     const onChangeMock = jest.fn()
-    const input = mount(<InputGroup value="value" onChange={onChangeMock} />).find('input')
+    const renderResult = render(<InputGroup value="value" onChange={onChangeMock} data-testid={'input-test'} />)
+    const inputElement = renderResult.getByTestId('input-test')
+    expect(inputElement).toBeInTheDocument()
 
     // Check if the default value for input is "text" and value is value
-    expect(input.prop('type')).toStrictEqual('text')
-    expect(input.prop('value')).toStrictEqual('value')
+    expect(inputElement).toHaveAttribute('type', 'text')
+    expect(inputElement).toHaveAttribute('value', 'value')
 
     const mockEvent = {
       preventDefault() {},
       target: { value: 'test-value' },
     } as React.ChangeEvent<HTMLInputElement>
-    input.simulate('change', mockEvent)
+    fireEvent.change(inputElement, mockEvent)
 
     expect(onChangeMock).toHaveBeenCalledTimes(1)
 
@@ -38,31 +41,31 @@ describe('<InputGroup>', () => {
   })
 
   it('supports custom type attribute', () => {
-    const emailInput = mount(<InputGroup type="email" />).find('input')
-    expect(emailInput.prop('type')).toStrictEqual('email')
+    const emailInput = render(<InputGroup type="email" data-testid={'email'} />).getByTestId('email')
+    expect(emailInput).toHaveAttribute('type', 'email')
 
-    const passwordInput = mount(<InputGroup type="password" />).find('input')
-    expect(passwordInput.prop('type')).toStrictEqual('password')
+    const passwordInput = render(<InputGroup type="password" data-testid={'password'} />).getByTestId('password')
+    expect(passwordInput).toHaveAttribute('type', 'password')
   })
 
   it('supports inputRef', () => {
     let input: HTMLInputElement | null = null
     // tslint:disable-next-line:jsx-no-lambda
-    mount(<InputGroup inputRef={(ref) => (input = ref)} />)
+    render(<InputGroup inputRef={(ref) => (input = ref)} />)
     expect(input).toBeInstanceOf(HTMLInputElement)
   })
 
   it('renders left icon before input', () => {
-    const input = mount(<InputGroup leftIcon="star" />).children()
-    expect(input.childAt(0).is(Icon)).toBeTruthy()
-    expect(input.childAt(1).hasClass(CssClasses.INPUT)).toEqual(true)
+    const renderResult = render(<InputGroup leftIcon="star" data-testid="input-test" />)
+    expect(renderResult.container.querySelector('[data-icon="star"]')).toBeInTheDocument()
+    expect(renderResult.container.querySelector('input').classList.contains(CssClasses.INPUT)).toEqual(true)
   })
 
   it(`renders right element inside .${CssClasses.INPUT_ACTION} after input`, () => {
-    const action = mount(<InputGroup rightElement={<address />} />)
-      .children()
-      .childAt(1)
-    expect(action.hasClass(CssClasses.INPUT_ACTION)).toEqual(true)
-    expect(action.find('address')).toHaveLength(1)
+    const renderResult = render(<InputGroup rightElement={<address />} data-testid="input-test" />)
+    const action = renderResult.container.querySelector('span')
+    expect(action).toBeInTheDocument()
+    expect(action.classList.contains(CssClasses.INPUT_ACTION)).toEqual(true)
+    expect(action.querySelectorAll('address')).toHaveLength(1)
   })
 })
