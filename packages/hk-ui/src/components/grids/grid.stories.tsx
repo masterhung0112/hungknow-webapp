9618c87a638f70a2c60ebf1bce0bb762e730d23e
hungbn <masterhung0112@gmail.com>
2021-08-11 22:57:46 +0700

    Working grid story

--- packages/hk-ui/src/components/grids/grid.stories.tsx
+++ packages/hk-ui/src/components/grids/grid.stories.tsx
@@ -4,18 +4,37 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 
 import './styles/_grids.scss';
+import './grid.stories.scss';
 
 storiesOf('Core/Components/Icon/React', module).
     add('default', () => {
         return (
-            <div>
+            <div className='hk-container'>
                 <div className='hk-row'>
-                    <div className='hk-column hk-column-50'>
+                    <div className='hk-col hk-col-50'>
                         <div className='content'>1/2</div>
                     </div>
-                    <div className='hk-column hk-column-50'>
+                    <div className='hk-col hk-col-50'>
                         <div className='content'>1/2</div>
                     </div>
+                </div>
+                <div className='hk-row'>
+                    <div className='hk-col hk-col-25'>
+                        <div className='content'>1/4</div>
+                    </div>
+                    <div className='hk-col hk-col-25'>
+                        <div className='content'>1/4</div>
+                    </div>
+                    <div className='hk-col hk-col-25'>
+                        <div className='content'>1/4</div>
+                    </div>
+                    <div className='hk-col hk-col-25'>
+                        <div className='content'>1/4</div>
+                    </div>
+                </div>
+                <div className='hk-row'>
+                    <div className='hk-col'><div className='content'>.col</div></div>
+                    <div className='hk-col hk-col-50 hk-col-offset-25'><div className='content'>.col col-50 col-offset-25</div></div>
                 </div>
             </div>
         );
