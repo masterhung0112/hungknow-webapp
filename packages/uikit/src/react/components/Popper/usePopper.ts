d20c92c87f771a004ca0f15884950af4a0799480
masterhung0112 <masterhung0112@gmail.com>
2022-04-15 20:30:31 +0700

    Update usePopper

--- packages/uikit/src/react/components/Popper/usePopper.ts
+++ packages/uikit/src/react/components/Popper/usePopper.ts
@@ -1,8 +1,17 @@
-import React, { useCallback, useMemo, useRef, useState } from "react";
-import Popper from "@popperjs/core";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import Popper, { createPopper } from "@popperjs/core";
 import { useSafeState } from "../../hooks/useSafeState";
+import { dequal } from "dequal";
+import { config } from "process";
+//import { createPopper } from "./popper";
 
-export type Modifier<Name, Options> = Popper.Modifier<Name, Options>
+export type Modifier<Name, Options> = Popper.Modifier<Name, Options>;
 export type Instance = Popper.Instance;
 export type Options = Popper.Options;
 export type Placement = Popper.Placement;
@@ -28,31 +37,74 @@ export type UsePopperOptions = Omit<
   modifiers?: Options["modifiers"];
 };
 
+const ariaDescribedByModifier: Modifier<"ariaDescribedBy", undefined> = {
+  name: "ariaDescribedBy",
+  enabled: true,
+  phase: "afterWrite",
+  effect:
+    ({ state }) =>
+    () => {
+      const { reference, popper } = state.elements;
+      if ("removeAttribute" in reference) {
+        const ids = (reference.getAttribute("aria-describedby") || "")
+          .split(",")
+          .filter((id) => id.trim() !== popper.id);
+
+        if (!ids.length) reference.removeAttribute("aria-describedby");
+        else reference.setAttribute("aria-describedby", ids.join(","));
+      }
+    },
+  fn: ({ state }) => {
+    const { popper, reference } = state.elements;
+
+    const role = popper.getAttribute("role")?.toLowerCase();
+
+    if (popper.id && role === "tooltip" && "setAttribute" in reference) {
+      const ids = reference.getAttribute("aria-describedby");
+      if (ids && ids.split(",").indexOf(popper.id) !== -1) {
+        return;
+      }
+
+      reference.setAttribute(
+        "aria-describedby",
+        ids ? `${ids},${popper.id}` : popper.id
+      );
+    }
+  },
+};
+
+const disabledApplyStylesModifier = {
+  name: 'applyStyles',
+  enabled: false,
+  phase: 'afterWrite',
+  fn: () => undefined,
+};
+
 /**
  * Position an element relative some reference element using Popper.js
- * @param param0 
- * @returns 
+ * @param param0
+ * @returns
  */
 export const usePopper = (
-    referenceElement: VirtualElement | null | undefined,
-    popperElement: HTMLElement | null | undefined,
-    { 
-        enabled = true,
-        placement = "bottom", 
-        strategy = 'absolute',
-        modifiers = []
-    }: UsePopperOptions = {}
+  referenceElement: VirtualElement | null | undefined,
+  popperElement: HTMLElement | null | undefined,
+  {
+    enabled = true,
+    placement = "bottom",
+    strategy = "absolute",
+    modifiers = [],
+  }: UsePopperOptions = {}
 ): UsePopperState => {
-  const prevModifiers = useRef<UsePopperOptions['modifiers']>(modifiers)
-  const popperInstanceRef = useRef<Instance>()
+  const prevModifiers = useRef<UsePopperOptions["modifiers"]>(modifiers);
+  const popperInstanceRef = useRef<Instance>();
 
   const update = useCallback(() => {
-      popperInstanceRef.current?.update()
-  }, [])
+    popperInstanceRef.current?.update();
+  }, []);
 
   const forceUpdate = useCallback(() => {
-      popperInstanceRef.current?.forceUpdate()
-  }, [])
+    popperInstanceRef.current?.forceUpdate();
+  }, []);
 
   const [popperState, setState] = useSafeState(
     useState<UsePopperState>({
@@ -67,33 +119,80 @@ export const usePopper = (
     })
   );
 
-  const updateModifier = useMemo<Modifier<'updateStateModifier', any>>(
-      () => ({
-          name: 'updateStateModifier',
-          enabled: true,
-          phase: 'write',
-          requires: ['computeStyles'],
-          fn: ({ state }) => {
-              const styles: UsePopperState['styles'] = {}
-              const attributes: UsePopperState['attributes'] = {}
+  const updateModifier = useMemo<Modifier<"updateStateModifier", any>>(
+    () => ({
+      name: "updateStateModifier",
+      enabled: true,
+      phase: "write",
+      requires: ["computeStyles"],
+      fn: ({ state }) => {
+        const styles: UsePopperState["styles"] = {};
+        const attributes: UsePopperState["attributes"] = {};
 
-              Object.keys(state.elements).forEach((element) => {
-                  styles[element] = state.styles[element]
-                    attributes[element] = state.attributes[element]
-              })
+        Object.keys(state.elements).forEach((element) => {
+          styles[element] = state.styles[element];
+          attributes[element] = state.attributes[element];
+        });
 
-              setState({
-                  state,
-                  styles,
-                  attributes,
-                  update,
-                  forceUpdate,
-                  placement: state.placement
-              })
-          }
-      }),
-      [update, forceUpdate, setState]
-  )
+        setState({
+          state,
+          styles,
+          attributes,
+          update,
+          forceUpdate,
+          placement: state.placement,
+        });
+      },
+    }),
+    [update, forceUpdate, setState]
+  );
+
+  const nextModifiers = useMemo(() => {
+    if (!dequal(prevModifiers.current, modifiers)) {
+      prevModifiers.current = modifiers;
+    }
+    return prevModifiers.current!;
+  }, [modifiers]);
+
+  useEffect(() => {
+    if (!popperInstanceRef.current || !enabled) return;
+
+    popperInstanceRef.current.setOptions({
+      placement,
+      strategy,
+      modifiers: [
+        ...nextModifiers,
+        updateModifier,
+        disabledApplyStylesModifier,
+      ],
+    });
+  }, [strategy, placement, updateModifier, enabled, nextModifiers]);
+  
+  useEffect(() => {
+    if (!enabled || referenceElement == null || popperElement == null) {
+      return undefined;
+    }
+    
+    popperInstanceRef.current = createPopper(referenceElement, popperElement, {
+      ...config,
+      placement,
+      strategy,
+      modifiers: [...nextModifiers, ariaDescribedByModifier, updateModifier],
+    });
+
+    return () => {
+      if (popperInstanceRef.current != null) {
+        popperInstanceRef.current.destroy();
+        popperInstanceRef.current = undefined;
+
+        setState((s) => ({
+          ...s,
+          attributes: {},
+          styles: { popper: {} },
+        }));
+      }
+    };
+  }, [enabled, referenceElement, popperElement]);
 
   return popperState;
 };
--- packages/uikit/test/react/usePopper.test.ts
+++ packages/uikit/test/react/usePopper.test.ts
@@ -1,7 +1,8 @@
 import { usePopper } from "../../src/react/components/Popper/usePopper";
 import { renderHook } from "@testing-library/react-hooks";
 // adds special assertions like toHaveTextContent
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
+// import { waitFor } from '@testing-library/react';
 
 describe("usePopper", () => {
   let elements: {
@@ -18,13 +19,30 @@ describe("usePopper", () => {
     elements.popper = document.createElement("div");
   });
 
-  it("should return state", () => {
+  it("should return state", async () => {
     const setupResult = setup();
+   
+    await setupResult.waitForNextUpdate()
     expect(setupResult.result.current.update).toBeInstanceOf(Function);
     expect(setupResult.result.current.forceUpdate).toBeInstanceOf(Function);
-    expect(setupResult.result.current.styles).toHaveAccessibleName("popper");
-    expect(setupResult.result.current.attributes).toHaveAccessibleName(
-      "popper"
+    expect(setupResult.result.current.styles).toHaveProperty("popper");
+    expect(setupResult.result.current.attributes).toHaveProperty("popper");
+  });
+
+  it("should add aria-describedBy for tooltips", async () => {
+    elements.popper.setAttribute("role", "tooltip");
+    elements.popper.setAttribute("id", "example123");
+
+    const setupResult = setup();
+    await setupResult.waitForNextUpdate()
+    expect(document.querySelector('[aria-describedby="example123"]')).toEqual(
+      elements.reference
+    );
+
+    setupResult.unmount();
+
+    expect(document.querySelector('[aria-describedby="example123"]')).toEqual(
+      null
     );
   });
 });
