e3642f66c5ded7eef7acf63d118c01c5f872b707
Hung Bui <masterhung0112@gmail.com>
2022-06-10 16:21:33 +0700

    use NavLink

--- packages/ss-root-nav/src/root.component.tsx
+++ packages/ss-root-nav/src/root.component.tsx
@@ -1,35 +1,39 @@
 import React from "react";
 import { navigateToUrl } from "single-spa";
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
+import { Button } from '@hungknow/uikit';
 
 export default function Root() {
   return (
-    <div className="hk1 navbar navbar-expand-lg">
-      <div className="navbar-brand">
-        <a href="/" className="hk-navbar-heading">
-          HungKnow
-        </a>
+    <BrowserRouter basename="/">
+      <div className="hk1 navbar navbar-expand-lg">
+        <div className="navbar-brand">
+          <a href="/" className="hk-navbar-heading">
+            Hung Know
+          </a>
+        </div>
+        <div className="hk1 collapse navbar-collapse" id="main-navbar-content">
+          <nav className="navbar-nav navbar-nav-scroll">
+            <a href="/" className="hk-button" onClick={navigateToUrl}>
+              Home
+            </a>
+            <NavLink to="/nft" className="hk-button" onClick={navigateToUrl}>
+              NFT
+            </NavLink>
+            <NavLink
+              to="/react-example"
+              className="hk-button"
+              onClick={navigateToUrl}
+            >
+              React Example
+            </NavLink>
+            <span className="hk-navbar-divider"></span>
+            <Button className="hk-button hk-minimal hk-icon-user"></Button>
+            <Button className="hk-button hk-minimal hk-icon-notification"></Button>
+            <Button className="hk-button hk-minimal hk-icon-cog"></Button>
+          </nav>
+        </div>
       </div>
-      <div className="hk1 collapse navbar-collapse" id="main-navbar-content">
-        <nav className="navbar-nav navbar-nav-scroll">
-          <a href="/" className="hk-button" onClick={navigateToUrl}>
-            Home
-          </a>
-          <a href="/nft" className="hk-button" onClick={navigateToUrl}>
-            NFT
-          </a>
-          <a
-            href="/react-example"
-            className="hk-button"
-            onClick={navigateToUrl}
-          >
-            React Example
-          </a>
-          <span className="hk-navbar-divider"></span>
-          <button className="hk-button hk-minimal hk-icon-user"></button>
-          <button className="hk-button hk-minimal hk-icon-notification"></button>
-          <button className="hk-button hk-minimal hk-icon-cog"></button>
-        </nav>
-      </div>
-    </div>
+    </BrowserRouter>
   );
 }
